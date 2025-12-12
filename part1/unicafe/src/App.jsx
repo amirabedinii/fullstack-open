9dfadca60909937113d6f6ecef47d7b7218e1a8c
amir sepehr abedini <amirsepehrabedini@amirs-MacBook-Pro-2.local>
2025-12-12 23:57:52 +0330

    add Button component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +16,13 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Button onClick={() => setGood(good + 1)} text='good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button onClick={() => setBad(bad + 1)} text='bad' />
+      <p>number of good {good}</p>
+      <p>number of neutral {neutral}</p>
+      <p>number of bad {bad}</p>
+      
     </div>
   )
 }
