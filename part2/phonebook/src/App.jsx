5d6226ac5ae7f1e191365ab1e75378ad788c5667
amir sepehr abedini <amirsepehrabedini@amirs-MacBook-Pro-2.local>
2025-12-14 12:08:30 +0330

    add part2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState("");
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
+    setNewName(event.target.value);
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    setPersons(persons.concat({ name: newName }))
-    setNewName('')
-  }
+    event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(
+        `${newName} is already in the phonebook, try again with a different name`
+      );
+      return;
+    }
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
 
   return (
     <div>
@@ -26,17 +29,19 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit" onClick={handleSubmit}>add</button>
+          <button type="submit" onClick={handleSubmit}>
+            add
+          </button>
         </div>
       </form>
       <h2>names</h2>
       <ul>
-        {persons.map(person => (
+        {persons.map((person) => (
           <li key={person.name}>{person.name}</li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
